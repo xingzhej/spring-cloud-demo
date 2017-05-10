1d36856f7f5395cf362057b69c96916b805ee0de
xingzhej <haomenbansui@126.com>
2017-05-10 09:33:34 +0800

    222222222222

--- demo-config-client/src/main/java/com/gsww/demo/config/DemoConfigClientApplication.java
+++ demo-config-client/src/main/java/com/gsww/demo/config/DemoConfigClientApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class DemoConfigClientApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(DemoConfigClientApplication.class, args);
 	}
 }
